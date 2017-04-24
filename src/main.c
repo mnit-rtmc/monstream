4b41b7139943ccabdc6e81c6ba8c2d217f442d6a
Douglas Lau (doug lau <Douglas Lau (doug.lau@state.mn.us)>
2017-04-24 13:01:49 -0500

    v0.8

--- src/main.c
+++ src/main.c
@@ -21,7 +21,7 @@
 #include <stdlib.h>
 #include "elog.h"
 
-#define VERSION "0.7"
+#define VERSION "0.8"
 #define BANNER "monstream: v" VERSION "  Copyright (C)  MnDOT\n"
 
 uint32_t load_config(void);
