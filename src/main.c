06dd7e85fe7037a73b931e9e3caf810f3806e426
Douglas Lau <doug.lau@state.mn.us>
2019-11-18 13:50:37 -0600

    v1.10

--- src/main.c
+++ src/main.c
@@ -17,7 +17,7 @@
 #include <string.h>
 #include <curl/curl.h>
 
-#define VERSION "1.10a1"
+#define VERSION "1.10"
 #define BANNER "monstream: v" VERSION "  Copyright (C) 2017-2019  MnDOT\n"
 
 void run_player(bool gui, bool stats, const char *port);
